f7df3cf72a09b56f69e1fcf3fbb3b1e4f5adc64f
Konrad Zdeb <konrad.zdeb@me.com>
2020-06-16 21:58:24 +0100

    Basic theme and package settings.

--- MaterialDesign/R/app_ui.R
+++ MaterialDesign/R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
@@ -8,27 +8,49 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
-    fluidPage(
-      h1("MaterialDesign")
+    # List the first level UI elements here
+    material_page(
+      title = "Material Design",
+      primary_theme_color = "#add8e6",
+      secondary_theme_color = "red",
+      material_side_nav(
+        fixed = TRUE,
+        # Place side-nav tabs within side-nav
+        material_side_nav_tabs(
+          side_nav_tabs = c(
+            "Example Side-Nav Tab 1" = "example_side_nav_tab_1",
+            "Example Side-Nav Tab 2" = "example_side_nav_tab_2"
+          ),
+          icons = c("cast", "insert_chart")
+        )
+      ),
+      # Define side-nav tab content
+      material_side_nav_tab_content(
+        side_nav_tab_id = "example_side_nav_tab_1",
+        tags$h1("First Side-Nav Tab Content")
+      ),
+      material_side_nav_tab_content(
+        side_nav_tab_id = "example_side_nav_tab_2",
+        tags$h1("Second Side-Nav Tab Content")
+      )
     )
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function(){
-  
+
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
@@ -36,7 +58,7 @@ golem_add_external_resources <- function(){
       app_title = 'MaterialDesign'
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
 
